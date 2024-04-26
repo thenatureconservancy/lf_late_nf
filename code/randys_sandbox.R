b929f83c32345c419d64f3e7603c4076edce1e4a
Randy Swaty <39503604+rswaty@users.noreply.github.com>
2024-04-26 11:38:37 -0400

    I think I got it with the simple data-see sandbox

--- code/randys_sandbox.R
+++ code/randys_sandbox.R
@@ -2,6 +2,30 @@
 
 ## Randy plays with joins
 
+df1 <- data.frame(
+  bps = c(1, 1, 1, 1, 1),
+  scl = c('a', 'b', 'c', 'd', 'e'),
+  ref = c(20, 20, 20, 20, 20),
+  join = c('1a', '1b', '1c', '1d', '1e')
+)
+
+df2 <- data.frame(
+  bps = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+  scl = c('a', 'b', 'c', 'e', 'a', 'b', 'c', 'e', 'a', 'b', 'c', 'e'),
+  cur = c(10, 20, 30, 40, 10, 20, 30, 40, 10, 20, 30, 40),
+  forest = c('h', 'h', 'h', 'h', 'z', 'z', 'z', 'z', 'm', 'm', 'm', 'm'),
+  join = c('1a', '1b', '1c', '1e', '1a', '1b', '1c', '1e', '1a', '1b', '1c', '1e')
+)
+
+
+# Create a dataset with all combinations of unique "join" and "forest" values
+all_combinations <- expand.grid(join = unique(df1$join), forest = unique(df2$forest))
+
+# Perform a left join to retain all rows from df1 and matching rows from df2
+df_merged <- left_join(all_combinations, df2, by = c("join", "forest"))
+
+
+
 
 
 
